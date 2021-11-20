8bc9112f0452d37878796a97521dc8a6b14ca067
ilsur-dev <me@ilsur.dev>
2021-11-21 03:52:25 +0500

    Получение событий с группировкой по дням (TimeSeries)

--- backend/endpoints/events/index.js
+++ backend/endpoints/events/index.js
@@ -1,8 +1,10 @@
 const [listParams, listHandler] = require('./list');
+const [timeSeriesParams, timeSeriesHandler] = require('./timeSeries');
 const [createParams, createHandler] = require('./create');
 
 module.exports = (fastify, ctx, done) => {
     fastify.get('/events/list', listParams, listHandler);
+    fastify.get('/events/timeseries', timeSeriesParams, timeSeriesHandler);
     fastify.post('/events/create', createParams, createHandler);
     fastify.register(require('./edit'));
     done();
---
+++ backend/endpoints/events/timeSeries.js
@@ -0,0 +1,50 @@
+const TIMESERIES_SQL = `
+    SELECT TO_CHAR(x, 'YYYY-MM-DD') "date",
+           (SELECT coalesce(json_agg(agg), '[]'::json) "events"
+            FROM (SELECT e.id,
+                         e.title,
+                         e.start_date,
+                         e.end_date,
+                         (SELECT coalesce(json_agg(t), '[]'::json)
+                          FROM events_tags et
+                                   JOIN tags t on t.id = et.tag_id
+                          WHERE et.event_id = e.id) tags
+                  FROM events e
+                  WHERE x::date >= e.start_date::date
+                    AND x::date <= e.end_date::date) agg)
+    FROM generate_series(date_trunc('month', $1::date), date_trunc('month', $1::date) + '1 month'::interval,
+                         '1 day'::interval) t(x)
+`;
+
+const isValidDate = (d) => {
+    return d instanceof Date && !isNaN(d);
+};
+
+async function handler(req, res) {
+    let date = isValidDate(new Date(req.query.date)) ? new Date(req.query.date) : new Date();
+    const {rows} = await req.pg.query(TIMESERIES_SQL, [date]);
+
+    const data = rows.reduce((accumulator, currentValue) => {
+        accumulator[currentValue.date] = currentValue.events;
+        return accumulator;
+    }, {});
+
+    console.log(data);
+    return Promise.resolve({statusCode: 200, data});
+}
+
+const params = {
+    schema: {
+        tags: ['events'],
+        summary: 'Получить список событий с группировкой по дням (TimeSeries)',
+        query: {
+            type: 'object',
+            properties: {
+                date: {type: 'string'}
+            }
+        },
+        response: {}
+    }
+};
+
+module.exports = [params, handler];
