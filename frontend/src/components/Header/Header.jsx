9c7219fb4ec431e8211826972dc343e34c05b708
VladosusCocosus <razin36986@gmail.com>
2021-11-21 08:14:49 +0500

    front

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -4,12 +4,18 @@ import {
 import "./header.css"
 
 const Header = () => {
+    const logout = () => {
+        localStorage.clear()
+        document.location.reload()
+
+    }
     return <div className={"header-body"}>
         <div className="header">
             <Link to="/">Главная</Link>
             <Link to="/admin">Административная панель</Link>
             <Link to="/admin/group">Группы</Link>
             <Link to="/admin/tags">Теги</Link>
+            <button onClick={() => logout()}>Выйти</button>
         </div>
     </div>
 }
